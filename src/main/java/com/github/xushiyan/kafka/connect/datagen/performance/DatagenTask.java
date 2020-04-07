fde8215cfe295c91a2d584946c9a8c289fac8c70
Jeff Zhang <zjffdu@apache.org>
2020-04-07 17:56:47 +0800

    add user-behavior

--- src/main/java/com/github/xushiyan/kafka/connect/datagen/performance/DatagenTask.java
+++ src/main/java/com/github/xushiyan/kafka/connect/datagen/performance/DatagenTask.java
@@ -24,6 +24,10 @@ import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -48,6 +52,9 @@ public class DatagenTask extends SourceTask {
     private Gson gson;
     private JsonObject messageTemplate;
 
+    // generate data from file
+    private BufferedReader fileReader;
+
     public String version() {
         return Version.get();
     }
@@ -56,6 +63,16 @@ public class DatagenTask extends SourceTask {
         this.config = new DatagenConnectorConfig(DatagenConnectorConfig.definition(), props);
         this.gson = new GsonBuilder().create();
         this.messageTemplate = this.gson.fromJson(this.config.messageTemplate, JsonObject.class);
+        List<String> randomFieldsValues = config.randomFields;
+        if (randomFieldsValues.size() == 1 && randomFieldsValues.get(0).startsWith("fromFile")) {
+            String[] splits = randomFieldsValues.get(0).split(":");
+            String fileName = splits[1];
+            try {
+                this.fileReader = new BufferedReader(new FileReader(fileName));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     private final Map<String, ?> sourcePartition = new HashMap<>();
@@ -71,39 +88,70 @@ public class DatagenTask extends SourceTask {
         List<SourceRecord> records = new ArrayList<>(config.pollSize);
         Random randomizer = new Random();
 
-        List<String> randomFieldsValues = config.randomFields;
-        Map<String, String[]> randomFieldsValueMap = new HashMap<>();
-        for (String kv : randomFieldsValues) {
-            String[] fieldAndValues = kv.split(":");
-            String fieldName = fieldAndValues[0];
-            String[] values = fieldAndValues[1].split(Pattern.quote("|"));
-            randomFieldsValueMap.put(fieldName, values);
-        }
-
-        for (int i = 0; i < config.pollSize; i++) {
-            JsonObject msg = msgTemplate.deepCopy();
-
-            for (Map.Entry<String, String[]> entry : randomFieldsValueMap.entrySet()) {
-                String[] values = entry.getValue();
-                if (entry.getValue().length == 1 && entry.getValue()[0].equals("random_ip")) {
-                    msg.addProperty(entry.getKey(), randomIP());
-                } else if (entry.getValue().length == 1 && entry.getValue()[0].equals("uuid")) {
-                    msg.addProperty(entry.getKey(), UUID.randomUUID().toString());
-                } else if (entry.getValue()[0].startsWith("random_int")) {
-                    int lowerBound = Integer.parseInt(entry.getValue()[1]);
-                    int upperBound = Integer.parseInt(entry.getValue()[2]);
-                    msg.addProperty(entry.getKey(), randomInt(lowerBound, upperBound));
-                } else {
-                    msg.addProperty(entry.getKey(), values[randomizer.nextInt(values.length)]);
-                }
+        // generate data randomly from template
+        if (fileReader == null) {
+            List<String> randomFieldsValues = config.randomFields;
+            Map<String, String[]> randomFieldsValueMap = new HashMap<>();
+            for (String kv : randomFieldsValues) {
+                String[] fieldAndValues = kv.split(":");
+                String fieldName = fieldAndValues[0];
+                String[] values = fieldAndValues[1].split(Pattern.quote("|"));
+                randomFieldsValueMap.put(fieldName, values);
             }
 
-            Instant now = Instant.now();
-            long nanos = TimeUnit.SECONDS.toNanos(now.getEpochSecond()) + now.getNano();
+            for (int i = 0; i < config.pollSize; i++) {
+                JsonObject msg = msgTemplate.deepCopy();
 
-            msg.addProperty(config.eventTimestampField, nanos);
+                for (Map.Entry<String, String[]> entry : randomFieldsValueMap.entrySet()) {
+                    String[] values = entry.getValue();
+                    if (entry.getValue().length == 1 && entry.getValue()[0].equals("random_ip")) {
+                        msg.addProperty(entry.getKey(), randomIP());
+                    } else if (entry.getValue().length == 1 && entry.getValue()[0].equals("uuid")) {
+                        msg.addProperty(entry.getKey(), UUID.randomUUID().toString());
+                    } else if (entry.getValue()[0].startsWith("random_int")) {
+                        int lowerBound = Integer.parseInt(entry.getValue()[1]);
+                        int upperBound = Integer.parseInt(entry.getValue()[2]);
+                        msg.addProperty(entry.getKey(), randomInt(lowerBound, upperBound));
+                    } else {
+                        msg.addProperty(entry.getKey(), values[randomizer.nextInt(values.length)]);
+                    }
+                }
 
-            records.add(new SourceRecord(sourcePartition, sourceOffset, config.topicName, Schema.STRING_SCHEMA, gson.toJson(msg)));
+                Instant now = Instant.now();
+                long nanos = TimeUnit.SECONDS.toNanos(now.getEpochSecond()) + now.getNano();
+
+                msg.addProperty(config.eventTimestampField, nanos);
+
+                records.add(new SourceRecord(sourcePartition, sourceOffset, config.topicName, Schema.STRING_SCHEMA, gson.toJson(msg)));
+            }
+        } else {
+            // generate data from file
+            for (int i = 0; i < config.pollSize; i++) {
+                JsonObject msg = msgTemplate.deepCopy();
+                Object[] fields =  msg.keySet().toArray();
+                String line = null;
+                try {
+                    line = fileReader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+                if (line == null) {
+                    return null;
+                }
+                String[] splits = line.split(",");
+                for (int j=0; j< fields.length; ++j) {
+                    msg.addProperty(fields[j].toString(), splits[j]);
+                }
+
+                if (config.eventTimestampField != null && config.eventTimestampField.length() > 0) {
+                    Instant now = Instant.now();
+                    long nanos = TimeUnit.SECONDS.toNanos(now.getEpochSecond()) + now.getNano();
+                    msg.addProperty(config.eventTimestampField, nanos);
+                }
+
+                records.add(new SourceRecord(sourcePartition, sourceOffset, config.topicName, Schema.STRING_SCHEMA, gson.toJson(msg)));
+            }
         }
 
         return records;
